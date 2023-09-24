c0ca4773f3c917dda66b0e2a243552213778ea3c
AgnieszkaCardozo <biuro@aga.olsztyn.pl>
2023-09-24 18:19:06 +0200

    Zadanie domowe dzien 3

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -10,16 +10,14 @@ if (sex == "Kobieta" && age < 30)
     Console.WriteLine("Kobieta poniżej 30 lat");
 }
 
-else
-{
-    if (name == "Ewa" && age == 33)
+else if (name == "Ewa" && age == 33)
 
         Console.WriteLine("Ewa, lat 33");
-}
-if (sex != "kobieta" && age < 18)
-{
+
+else if (sex != "kobieta" && age < 18)
+
     Console.WriteLine("Niepełnoletni mężczyzna");
-}
+
 
 if (name != "Ewa" || age > 50 && sex == "Kobieta")
 { Console.WriteLine("Staruszka"); }
